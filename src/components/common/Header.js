9f13307a66f4680e6347992657c912d2fd8c32df
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-11-22 17:32:36 +0100

    added basic header/navbar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,43 @@
 import React, { PropTypes } from 'react'
-import { Link, IndexLink } from 'react-router'
+import { withRouter } from 'react-router'
+import { uppercaseFirst } from '../../selectors/selectors'
+import './Header.scss'
 
-const Header = () => {
-  return (
-    <nav className="Header">
-      This is working
-    </nav>
-  )
+class Header extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = {
+      search: ''
+    }
+
+    this.change = this.change.bind(this)
+    this.redirect = this.redirect.bind(this)
+  }
+  change(event) {
+    this.setState({search: event.target.value})
+  }
+  redirect (event) {
+    event.preventDefault()
+    this.props.router.push(`/${this.state.search.toLowerCase()}`)
+  }
+  render () {
+    return (
+      <nav className="Header">
+        <form onSubmit={this.redirect}>
+          <input className="Header_search" placeholder="Enter city name" value={this.state.search} onChange={this.change} />
+          <input className="Header_submit" type="submit" value="search" />
+        </form>
+        <h1>{uppercaseFirst(this.props.params.country || 'Gibraltar')}</h1>
+      </nav>
+    )
+  }
 }
 
 Header.propTypes = {
+  router: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired
 }
 
-export default Header
+export default withRouter(Header)
