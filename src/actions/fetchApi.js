18074ede5049d3b6dcbc1d14f4ef0318fb148c3a
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-11-22 16:32:42 +0100

    added functions for fetching data from weather api

---
+++ src/actions/fetchApi.js
@@ -0,0 +1,22 @@
+import * as types from './actionTypes'
+import axios from 'axios'
+
+const apiEntrypoint = axios.create({
+  baseURL: 'http://api.openweathermap.org/data/2.5/',
+  timeout: 2000,
+  params: {
+    appid: "d5fa9909b8944d603b81a6a7dbec6b13"
+  }
+});
+
+export function getWeather(cityName) {
+  return apiEntrypoint.get("weather", {
+    params: {q: cityName}
+  })
+}
+export function getForecast(cityName) {
+  return apiEntrypoint.get("forecast", {
+    params: {q: cityName}
+  })
+}
+//(axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d5fa9909b8944d603b81a6a7dbec6b13`)
