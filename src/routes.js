41a94a07f6385c38ddeee49c3a4c34a582297d7d
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-11-22 16:39:16 +0100

    added CountryPage to routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout'
+import CountryPageManager from './components/countryPage/CountryPageManager'
 
 export default (
-  <Route path="/" component={Layout} />
+  <Route path="/" component={Layout} >
+    <IndexRoute component={CountryPageManager} />
+    <Route path="(:country)" component={CountryPageManager} />
+  </Route>
 )
