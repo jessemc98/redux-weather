44d88f9dc9c8cf0d7c3e39fedd5eb8abcf47bef4
Jesse McIntosh <jessemc98@hotmail.com>
2017-03-14 17:12:59 +0100

    added citySearch reducer

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,4 +1,5 @@
 export default {
   overview: {},
-  forecast: {}
+  forecast: {},
+  citySearch: []
 }
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
-import overview from './overviewReducer.js'
-import forecast from './forecastReducer.js'
+import overview from './overviewReducer'
+import forecast from './forecastReducer'
+import citySearch from './citySeachReducer'
 
 const rootReducer = combineReducers({
   overview,
-  forecast
+  forecast,
+  citySearch
 })
 
 export default rootReducer;
